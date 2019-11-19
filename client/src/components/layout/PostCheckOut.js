2ef05c555be9ce970d61112e5e6223af15b6fe6e
leilaadel <leilaadel96@gmail.com>
2019-11-19 13:47:56 -0800

    found api link to photos

--- client/src/components/layout/PostCheckOut.js
+++ client/src/components/layout/PostCheckOut.js
@@ -10,17 +10,20 @@ class PostCheckOut extends Component {
     axios.get(
         'https://photoswami.com/api/library/', {
             headers: {
-            'Authorization' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZU51bWJlciI6Ijg1ODQxNDc1NDUiLCJpYXQiOjE1NzM3NjUyNzksImV4cCI6MTU3Mzc2ODg3OX0.qgUoVe8Y_yMLmVe554KlcXoV2nz_qAqjspt_P6G4BfQ'
+            'Authorization' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZU51bWJlciI6IjE4NTg0MTQ3NTQ1IiwiaWF0IjoxNTc0MTk5ODc1LCJleHAiOjE1NzQyMDM0NzV9.0pmdVFLV2z29jI1RgmSBhRjcXIW3lUWtqGoI0pAWFjM'
           }
         }
       )
       .then((response) => {
           var response = response.data;
           console.log(response);
+          console.log("success");
+          console.log(response.results);
         },
         (error) => {
           var status = error.response.status
           console.log(status);
+          console.log("error");
         }
       );
     }
@@ -75,30 +78,29 @@ class PostCheckOut extends Component {
         <h5 style={{textAlign: "left"}}>November 2019</h5>
         <div class="photos">
             <div class="photo-block" style={{width: "100%"}}>
-                <img src="https://cdn.pixabay.com/photo/2016/11/08/05/08/adult-1807500_960_720.jpg" alt="photoswami"/>
-                <button type="button">
-                    <p>$5</p>
-                <label class="container">
-                        <input type="checkbox"/>
-                        <span class="checkmark"></span>
-                </label>
-                </button>
+                <img src="http://res.cloudinary.com/midnight-oil-developers/image/private/c_scale,w_300/a_19,co_rgb:b8a859,fl_tiled,l_text:Michroma_20_bold:photoSwami.com,o_80/v1/5dcdd4a7a2773527d1a35db0/20191114_022647_0" alt="photoswami"/>
+                {/* <div class="price-box"></div> */}
+                    <label class="container1">
+                        <input type="checkbox"/>  
+                        <span class="checkmark">$1</span>
+                    </label>
+                {/* </div> */}
             </div>
             <div class="photo-block" style={{width: "100%"}}>
-                <img src="https://images.unsplash.com/photo-1496275068113-fff8c90750d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="photoswami"/>
-                <label class="container">
-                        <input type="checkbox"/>
-                        <span class="checkmark"></span>
-                </label>
+                <img src="http://res.cloudinary.com/midnight-oil-developers/image/private/c_scale,w_300/a_19,co_rgb:b8a859,fl_tiled,l_text:Michroma_20_bold:photoSwami.com,o_80/v1/5dcdd4a7a2773527d1a35db0/20191114_022649_1" alt="photoswami"/>
+                        <label class="container1">
+                        <input type="checkbox"/>  
+                        <span class="checkmark">$1</span>
+                    </label>
             </div>
             <div class="photo-block" style={{width: "100%"}}>
-                <img src="https://images.unsplash.com/photo-1518159357049-bb438a340932?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="photoswami"/>
-                <label class="container">
-                        <input type="checkbox"/>
-                        <span class="checkmark"></span>
+                <img src="http://res.cloudinary.com/midnight-oil-developers/image/private/c_scale,w_300/a_19,co_rgb:b8a859,fl_tiled,l_text:Michroma_20_bold:photoSwami.com,o_80/v1/5dcdd4a7a2773527d1a35db0/20191114_022651_2" alt="photoswami"/>
+                <label class="container1">
+                        <input type="checkbox"/>  
+                        <span class="checkmark">$1</span>
                 </label>
             </div>
-    </div>
+    {/* </div> */}
         <div className="row">
             {/* <form>
                 <div class="select-image">
@@ -142,9 +144,9 @@ class PostCheckOut extends Component {
         <h5 style={{textAlign: "left"}}>October 2019</h5>
         <div class="photo-block" style={{width: "100%"}}>
             <img src="https://images.unsplash.com/photo-1511895426328-dc8714191300?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="photoswami"/>
-            <label class="container">
-                    <input type="checkbox"/>
-                    <span class="checkmark"></span>
+            <label class="container1">
+                        <input type="checkbox"/>  
+                        <span class="checkmark"></span>
             </label>
         </div>
         <button type="button" style={{
@@ -181,6 +183,7 @@ class PostCheckOut extends Component {
                 </div> */}
             </div>
         </div>
+    </div>
     </div>
     );
   }
